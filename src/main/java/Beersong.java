f09bdc0c5a7a6a86c9f156ca0e9aacf6204700d8
Nigel McIntosh <nwm2000@me.com>
2022-03-14 14:29:25 -0400

    added

--- src/main/java/Beersong.java
+++ src/main/java/Beersong.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Beersong {
+public class Beersong {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+
+            if (beerNum == 1) {
+                word = "bottle";
+            }
+
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+            System.out.println("take one down.");
+            System.out.println("pass it around.");
+            beerNum = beerNum - 1;
+
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + " of beer on the wall");
+            } else {
+                System.out.println("no more bottles of beer on the wall");
+            }
+        }
+    }
 }
